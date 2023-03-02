a823e076e921848ad9e50f1e1638bfeee3d4d860
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-03-02 18:06:06 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -64,8 +64,8 @@ resource "azurerm_public_ip" "app_public_ip" {
 }
 resource "azurerm_network_security_group" "example" {
   name                = "example-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.app_grp.location
+  resource_group_name = azurerm_resource_group.app_grp.name
 
   security_rule {
     name                       = "SSH"
