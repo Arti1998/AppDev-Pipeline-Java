b62f229cc07a479313c071e9972ff2f5cb6b62b7
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-03-03 22:53:17 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -79,6 +79,17 @@ resource "azurerm_network_security_group" "example" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+   security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "app_interface" {
