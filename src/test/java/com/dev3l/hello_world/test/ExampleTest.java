e579e8859b488f2876a6311f1c6eb5eb33cfb839
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-03-03 21:21:28 +0530

    Update ExampleTest.java

--- src/test/java/com/dev3l/hello_world/test/ExampleTest.java
+++ src/test/java/com/dev3l/hello_world/test/ExampleTest.java
@@ -17,26 +17,7 @@ import java.io.IOException;
 
 public class ExampleTest {
 	
-	@Test
-		  public void testWebPage() throws IOException {
-		    // Make a request to the web page and get the response
-		    URL url = new URL("http://localhost:8085/JavaApp/"); // replace with the URL of your web app
-		    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		    con.setRequestMethod("GET");
-		    int responseCode = con.getResponseCode();
-		    assertEquals(200, responseCode); // check that the response code is 200 OK
-
-		    // Read the response content and check for the title, body, image, and header
-		    Scanner scanner = new Scanner(con.getInputStream());
-		    String responseBody = scanner.useDelimiter("\\A").next();
-		    scanner.close();
-
-		    assertTrue(responseBody.contains("<title>Hello World! First Java Pipeline</title>")); // check for the title tag
-		    assertTrue(responseBody.contains("<h2 class=\"text-center\">Hello World! First Java Pipeline V0.1</h2>")); // check for the header
-		    assertTrue(responseBody.contains("<p class=\"text-center\">Now with Arti K-CI-CD!New Change</p>")); // check for the body
-		    assertTrue(responseBody.contains("<img src=\"https://octodex.github.com/images/spidertocat.png\" alt=\"Spidertocat\"")); // check for the image
-		  }
-
+	
 	 @Test
 		public void testWebPageAgain() throws IOException {
 		    // Read the content of the JSP file
