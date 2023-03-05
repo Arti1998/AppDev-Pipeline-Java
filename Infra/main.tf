b335a80e6192ccf74d3952cdf565a55520a078e7
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-03-06 00:15:55 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -157,6 +157,16 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
       "sudo apt-get install -y docker.io",
       "sudo systemctl enable docker",
       "sudo systemctl start docker"
+      
+      "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
+      "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni",
+      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16",
+      "mkdir -p $HOME/.kube",
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+      "kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml",
     ]
 
     connection {
