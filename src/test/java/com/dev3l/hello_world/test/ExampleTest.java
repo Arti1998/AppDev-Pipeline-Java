1d7648ba235dd802120a1839b0e82ada470d9d68
Arti1998 <38453172+Arti1998@users.noreply.github.com>
2023-03-07 12:14:15 +0530

    Update ExampleTest.java

--- src/test/java/com/dev3l/hello_world/test/ExampleTest.java
+++ src/test/java/com/dev3l/hello_world/test/ExampleTest.java
@@ -33,8 +33,8 @@ public class ExampleTest {
 
 		    // Check for the title, header, body, and image tags
 		    assertTrue(content.contains("<title>Hello World! First Java Pipeline</title>"));
-		    assertTrue(content.contains("<h2 class=\"text-center\">Hello World! First Java Pipeline V0.1</h2>"));
-		    assertTrue(content.contains("<p class=\"text-center\">Now with Arti K-CI-CD!New Change</p>"));
+		    assertTrue(content.contains("<h2 class=\"text-center\">Hello World! First Java Pipeline V0.2</h2>"));
+		    assertTrue(content.contains("<p class=\"text-center\">Now with CS K-CI-CD!New Change</p>"));
 		    assertTrue(content.contains("<img src=\"https://octodex.github.com/images/spidertocat.png\" alt=\"Spidertocat\""));
 		}
 	
